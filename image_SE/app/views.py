5b9db5152b3db9269e2a5034357f9fe92d791232
Md Asjad <mdasjad895@gmail.com>
2023-05-07 00:40:26 +0530

    updated

--- image_SE/app/views.py
+++ image_SE/app/views.py
@@ -7,14 +7,7 @@ import pandas as pd
 from django.contrib import messages
 from django.shortcuts import render
 from django.conf import settings
-import tensorflow as tf
-from django.http import HttpResponse
-from tensorflow import keras
-from keras.models import Model
-from keras.models import load_model
-from keras.utils import load_img, img_to_array
-import csv
-import numpy as np
+
 
 def search(request):
     if request.method == 'POST' and request.FILES['query_image']:
@@ -23,7 +16,30 @@ def search(request):
         k = request.GET.get('k')
         q = Query.objects.create(image=query_image,k=k)
         print(query_image)
-        d,rt,cap=retrived(query_image)
+        import requests
+
+# Set the URL of your deployed Streamlit app
+        url = 'https://your-streamlit-app-url/predict_endpoint'
+
+# Set the image file path
+        image_path = 'path/to/your/image.jpg'
+
+# Open the image file and read its contents as binary data
+        with open(image_path, 'rb') as f:
+             image_data = f.read()
+
+# Set the request headers
+        headers = {'Content-Type': 'application/octet-stream'}
+
+# Set the request data
+        data = {'image': image_data}
+
+# Make the prediction request
+        response = requests.post(url, headers=headers, data=data)
+
+# Get the prediction from the response
+        prediction = response.json()
+
         # Render the template
         messages.success(request,{'query_image': query_image, 'retrieved_paths': rt})
         return render(request, 'result.html', {'query_image': query_image, 'retrieved_paths': rt})
